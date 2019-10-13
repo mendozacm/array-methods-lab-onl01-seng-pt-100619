690e77ae17a2c529354c7abb63e120398579108c
Cesar Mendoza <mendoza.cesar92@gmail.com>
2019-10-13 01:55:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,20 +8,21 @@ famous_cats.sort
 end
 
 def using_reverse(array)
-famous_wizards = ["Dumbledore", "Gandalf", "Merlin"]
+famous_wizards = ["wow", "I", "am", "really", "learning", "arrays!"]
 famous_wizards.reverse
 end
 
 def using_first(array)
-famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+famous_cats = ["wow", "I", "am", "really", "learning", "arrays!"]
 famous_cats.first
 end
 
 def using_last(array)
-famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+famous_cats = ["wow", "I", "am", "really", "learning", "arrays!"]
 famous_cats.last
 end
 
 def using_size(array)
-
+famous_cats = ["wow", "I", "am", "really", "learning", "arrays!"]
+famous_cats.size
 end
